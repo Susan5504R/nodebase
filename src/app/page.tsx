0aad5159e792455f4efa8b2a0190ae3dc4310cc4
Sarah <sarahsagri02@gmail.com>
2026-01-20 17:24:40 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button";
+const Page = () => {
+  return (
+    <div>
+      <Button variant={"outline"}>Click Me </Button>
+      Hello World
+    </div>
+  )
+};
+export default Page;
